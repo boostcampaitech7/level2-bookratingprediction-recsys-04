5fa823da8bb10cf2d32c0d430c924fa8a03bd983
Seongyun <seongyungim99@gmail.com>
2024-11-06 11:55:23 +0900

    feat: Add basic settings

---
+++ src/data/__init__.py
@@ -0,0 +1,3 @@
+from .basic_data import basic_data_load, basic_data_split, basic_data_loader
+from .text_data import text_data_load, text_data_split, text_data_loader
+from .combined_data import combined_data_load, combined_data_split, combined_data_loader
