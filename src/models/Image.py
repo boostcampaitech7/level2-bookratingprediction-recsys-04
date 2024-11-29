f847699e991717708aedca4a835efebcdf7a0abc
gagoory7 <gagoory7@gmail.com>
2024-11-29 05:21:24 +0000

    fix: Fix VGGNet training issue

--- src/models/Image.py
+++ src/models/Image.py
@@ -8,8 +8,10 @@ class VGGNet(Base_Image_DeepFM):
     def __init__(self, args, data):
         # VGG16 모델 로드
         vgg = torch.hub.load('pytorch/vision:v0.9.0', 'vgg16', weights=models.VGG16_Weights.IMAGENET1K_V1)
-        vgg.classifier = nn.Identity()
+        vgg.classifier = nn.Identity() 
+        
         super().__init__(args, data, image_model=vgg, image_feature_dim=25088)
+        self.image_model.vgg_embedding = nn.Linear(25088, self.embed_dim)  # 25088에서 embed_dim으로 변환
 
 class ResNet(Base_Image_DeepFM):
     def __init__(self, args, data):
